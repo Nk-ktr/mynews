d497964bf118f490ac6a43c0a78390261364590b
kotaro <dash-life.21@i.softbank.jp>
2020-04-29 12:06:22 +0000

    4月29日課題

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+});
+
+//課題3//
+
+Route::get('XXX','Admin\AAAController@BBB');
+
+//課題4//
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create','Admin\ProfileController@add');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
