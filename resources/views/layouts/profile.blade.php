342647fb870298309659a56b8b4f0c03842885c9
kotaro <dash-life.21@i.softbank.jp>
2020-05-04 15:58:54 +0000

    Laravel11 課題

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -23,7 +23,7 @@
         
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         
-        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
